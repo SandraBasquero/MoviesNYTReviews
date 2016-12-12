b0bf844e1f1d03b123225e50d4e2ccedacc76261
SandraBasquero <sandrabasquero@gmail.com>
2016-12-12 16:44:06 +0100

    TableView

---
+++ MoviesNYTReviews/Pods/Target Support Files/Pods-MoviesNYTReviewsTests/Pods-MoviesNYTReviewsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MoviesNYTReviewsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MoviesNYTReviewsTestsVersionString[];
+
---
+++ MoviesNYTReviews/Pods/Target Support Files/Pods-MoviesNYTReviewsUITests/Pods-MoviesNYTReviewsUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MoviesNYTReviewsUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MoviesNYTReviewsUITestsVersionString[];
+
