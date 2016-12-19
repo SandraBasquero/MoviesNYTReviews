100d3b44ba8711c37b25032d215597be2c0219da
SandraBasquero <sandrabasquero@gmail.com>
2016-12-19 19:01:50 +0100

    Test: get a specific data from JSON

--- MovieNYTReviews/MovieNYTReviews/ApiManager.swift
+++ MovieNYTReviews/MovieNYTReviews/ApiManager.swift
@@ -7,35 +7,24 @@
 //
 
 import UIKit
+import Foundation
 import Alamofire
 import SwiftyJSON //In Terminal to fix error: carthage update --platform iOS --no-use-binaries
 
 class ApiManager: NSObject {
     
     func getTest() {
-        //var myJson:Data = Data()
+       // var myJson:[String: Any]?
         Alamofire.request("https://api.nytimes.com/svc/movies/v2/reviews/search.json?&api-key=0f74e88f53854f4687876afdb617a208").responseJSON { response in
-            /*print(response.request as Any)  // original URL request
-            print(response.response as Any) // HTTP URL response
-            print(response.data as Any)     // server data
-            print(response.result)   // result of response serialization*/
             
-            if let JSONN = response.result.value {
-                print("JSON: \(JSONN)")
-                //myJson = JSON as! Data
-                let myJson = JSON(JSONN)
-                /*if let userName = myJson[0][0][0] {
-                    print(userName)
-                }*/
+            if let JSONN = response.result.value as? [String: Any] {
+                //print("JSON: \(JSONN["results"])")
+                let resultss:NSArray = JSONN["results"] as! NSArray
+                //print(resultss[0])
+                let dictio:NSDictionary = resultss[0] as! NSDictionary
+                print(dictio.value(forKey: "display_title")!)
             }
         }
-        
-     /*   let json = JSON(data: myJson)
-        if json[0]["display_title"].string != nil {
-            //Now you got your value
-            print(json[0]["display_title"].string!)
-        }*/
-        
     }
     
 }
