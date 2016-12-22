e68a3db10b4f7106cc704e42d30bff69e4a01824
Sandra Basquero <sbs_epsfactoria@iecisa.com>
2016-12-22 18:15:38 +0100

    Better way to get values from JSON
    Alamofire + SwiftyJSON

--- MovieNYTReviews/MovieNYTReviews/ApiManager.swift
+++ MovieNYTReviews/MovieNYTReviews/ApiManager.swift
@@ -16,16 +16,15 @@ class ApiManager: NSObject {
     let apiKey:String = "0f74e88f53854f4687876afdb617a208"
     
     func getTest() {
-       // var myJson:[String: Any]?
-        Alamofire.request("https://api.nytimes.com/svc/movies/v2/reviews/search.json?&api-key="+apiKey).responseJSON { response in
-            
-            if let JSONN = response.result.value as? [String: Any] {
-                //print("JSON: \(JSONN["results"])")
-                let resultss:NSArray = JSONN["results"] as! NSArray
-                //print(resultss[0])
-                let dictio:NSDictionary = resultss[0] as! NSDictionary
-                print(dictio.value(forKey: "display_title")!)
-                print(dictio.value(forKey: "opening_date")!)
+        let url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?&api-key="+apiKey
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let results = json["results"].arrayValue
+                print("\(results[0]["display_title"].stringValue)")
+            case .failure(let error):
+                print(error)
             }
         }
     }
