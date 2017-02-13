70a6322320d1fd375dc42d815288b803f66f073c
X50207BA <x50207ba@MX3500906DC2020.local>
2017-02-13 18:37:09 +0100

    Trying to fix the Activity Indicator in DetailVC

--- MovieNYTReviews/MovieNYTReviews/ViewControllers/DetailVC.swift
+++ MovieNYTReviews/MovieNYTReviews/ViewControllers/DetailVC.swift
@@ -16,31 +16,34 @@ class DetailVC: UIViewController {
     
     @IBOutlet weak var articleWebView: UIWebView!
     var urlString = ""
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        //self.activity.startAnimating()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.activity.startAnimating()
         
         self.subtitleDetail.text = subtitleFromList
         
         let url = NSURL (string: urlString)
         let requestObj = URLRequest(url: url! as URL)
-        self.articleWebView.loadRequest(requestObj)
-        
+        //self.articleWebView.loadRequest(requestObj)
+
+        loadURL(requestObj, completion:{
+            success in
+            self.activity.stopAnimating()
+        })
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loadURL(_ url: URLRequest, completion:(Bool?)->Void) {
+        self.articleWebView.loadRequest(url)
+        completion(true)
     }
-    */
 
 }
