91957724bfe73ef806f4036464cfc90ecd6c7993
Sandra <sandrabasquero@gmail.com>
2017-03-18 18:43:24 +0100

    Activity Indicator while film review URL is loading in Web View.

--- MovieNYTReviews/MovieNYTReviews/ViewControllers/DetailVC.swift
+++ MovieNYTReviews/MovieNYTReviews/ViewControllers/DetailVC.swift
@@ -9,42 +9,37 @@
 import UIKit
 
 
-class DetailVC: UIViewController {
+class DetailVC: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var subtitleDetail: UILabel!
     var subtitleFromList:String?
-    
     @IBOutlet weak var articleWebView: UIWebView!
     var urlString = ""
     @IBOutlet weak var activity: UIActivityIndicatorView!
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        //self.activity.startAnimating()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationTitleStyle()
         
+        self.articleWebView.delegate = self
+        
         self.activity.startAnimating()
         self.subtitleDetail.text = subtitleFromList
         
+        //Loading film review URL in UIWebView
         let url = NSURL (string: urlString)
         let requestObj = URLRequest(url: url! as URL)
-        //self.articleWebView.loadRequest(requestObj)
-        loadURL(requestObj, completion:{
-            success in
-            self.activity.stopAnimating()
-        })
+        self.articleWebView.loadRequest(requestObj)
     }
     
-    func loadURL(_ url: URLRequest, completion:(Bool?)->Void) {
-        self.articleWebView.loadRequest(url)
-        completion(true)
+    //Stop and hide Activity Indicator
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        self.activity.stopAnimating()
+        self.activity.isHidden = true
     }
     
+    // Title style
     func navigationTitleStyle() {
         let frame = CGRect(x: 0, y: 0, width: 200, height: 40)
         let tlabel = UILabel(frame: frame)
