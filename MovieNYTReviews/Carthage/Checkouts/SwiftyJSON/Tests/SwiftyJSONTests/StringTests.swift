2d729023d2079c1f371798fc2f5b29f3dc950db4
Sandra <sandrabasquero@gmail.com>
2018-07-28 13:21:24 +0200

    Fix dependencies issues caused by new Swift versions.

--- MovieNYTReviews/Carthage/Checkouts/SwiftyJSON/Tests/SwiftyJSONTests/StringTests.swift
+++ MovieNYTReviews/Carthage/Checkouts/SwiftyJSON/Tests/SwiftyJSONTests/StringTests.swift
@@ -1,6 +1,6 @@
 //  StringTests.swift
 //
-//  Copyright (c) 2014 - 2016 Pinglin Tang
+//  Copyright (c) 2014 - 2017 Pinglin Tang
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -35,10 +35,10 @@ class StringTests: XCTestCase {
         XCTAssertEqual(json.string!, "12345?67890.@#")
         XCTAssertEqual(json.stringValue, "12345?67890.@#")
     }
-    
+
     func testUrl() {
         let json = JSON("http://github.com")
-        XCTAssertEqual(json.url!, URL(string:"http://github.com")!)
+        XCTAssertEqual(json.url!, URL(string: "http://github.com")!)
     }
 
     func testBool() {
@@ -53,6 +53,16 @@ class StringTests: XCTestCase {
 
     func testBoolWithT() {
         let json = JSON("T")
+        XCTAssertTrue(json.boolValue)
+    }
+
+    func testBoolWithYes() {
+        let json = JSON("Yes")
+        XCTAssertTrue(json.boolValue)
+    }
+
+    func testBoolWith1() {
+        let json = JSON("1")
         XCTAssertTrue(json.boolValue)
     }
 
